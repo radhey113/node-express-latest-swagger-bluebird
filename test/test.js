2506aac53c4fb6d48885b8b0e7489120474a01e6
radheyg11 <radheyg11@gmail.com>
2019-01-20 23:46:14 +0530

    test case directory added

---
+++ test/test.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+var expect  = require(`chai`).expect;
+var request = require(`request`);
+
+it(`Main page content`, (done) => {
+    request('http://localhost:2201' , (error, response, body) => {
+        expect('Hello World').to.equal('Hello World');
+        done();
+    });
+});
